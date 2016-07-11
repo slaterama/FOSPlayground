998921b503c9628e001f9026b46a60544dd44b18
Scott Slater <slaterama@gmail.com>
2016-07-10 21:02:00 -0400

    First pass of FOSOAuthServerBundle integration

---
+++ src/ApiBundle/Entity/AuthCode.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: slaterama
+ * Date: 7/9/16
+ * Time: 9:56 AM
+ */
+
+namespace ApiBundle\Entity;
+
+use FOS\OAuthServerBundle\Entity\AuthCode as BaseAuthCode;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Table("oauth2_auth_codes")
+ * @ORM\Entity
+ */
+class AuthCode
+{
+	/**
+	 * @ORM\Id
+	 * @ORM\Column(type="integer")
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 */
+	protected $id;
+
+	/**
+	 * @ORM\ManyToOne(targetEntity="Client")
+	 * @ORM\JoinColumn(nullable=false)
+	 */
+	protected $client;
+
+	/**
+	 * @ORM\ManyToOne(targetEntity="AppBundle\Entity\User")
+	 */
+	protected $user;
+}
